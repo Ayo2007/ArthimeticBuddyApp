1760247e02be393780e9f79349ea4ae43b9dc6c5
Ayo <aogunkinle@lcs.on.ca>
2024-02-03 21:43:05 -0500

    Initial Commit

---
+++ ArthimeticBuddyApp/ArthimeticBuddyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArthimeticBuddyAppApp.swift
+//  ArthimeticBuddyApp
+//
+//  Created by Ayodeji Ogunkinle on 2024-02-03.
+//
+
+import SwiftUI
+
+@main
+struct ArthimeticBuddyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
